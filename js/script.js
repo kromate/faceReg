88cf10b9d73597e3a86e092147d1f04878e276b1
kromate <akpananthony33@gmail.com>
2020-07-02 01:32:22 +0100

    s

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ Promise.all([
   faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
   faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
   faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('/models')
+  faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
 ]).then(startVideo)
 
 function startVideo() {
@@ -39,29 +39,28 @@ video.addEventListener('play', () => {
   const displaySize = { width: video.width, height: video.height }
   faceapi.matchDimensions(canvas, displaySize)
   var recog = setInterval(async () => {
-    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks()
     const resizedDetections = faceapi.resizeResults(detections, displaySize)
     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
     faceapi.draw.drawDetections(canvas, resizedDetections)
     faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-    faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
     if (resizedDetections.length > 0) {
       let img = document.querySelector('#img')
       takeSnapShot()
       if (!img) {
         console.log('snap')
-        
+
         document.querySelector('#alert').style.color = 'green';
         document.querySelector('#alert').innerHTML = `Face Found Scaning`
 
         clearInterval(recog)
       } else {
-        
+
         document.querySelector('#alert').style.color = 'red';
         document.querySelector('#alert').innerHTML = `Can't find a Face, Please Adjust`
-      } 
+      }
 
-    } else { console.log('adjust your face')}
+    } else { console.log('adjust your face') }
   }, 1000)
 });
 
@@ -80,6 +79,38 @@ video.addEventListener('play', () => {
 //   window.location.assign('./index.html')
 // }
 
-function scanImg(){
-  console.log('snapped')
+async function scanImg() {
+
+  let image = document.querySelector('#img');
+  const container = document.createElement('div')
+  container.style.position = 'relative'
+  document.body.append(container)
+  const canvas = faceapi.createCanvasFromMedia(image)
+  document.body.append(canvas)
+  const displaySize = { width: image.width, height: image.height }
+  faceapi.matchDimensions(canvas, displaySize)
+  const detection = await faceapi.detectAllFaces(image).withFaceLandmarks().withFaceDescriptors()
+  if (detection.length < 1) {
+    recapture()
+  } else { 
+    console.log(detection.length)
+    const resizedDetections = faceapi.resizeResults(detection, displaySize)
+    resizedDetections.forEach((detection) => {
+      const box = detection.detection.box
+      const drawBox = new faceapi.draw.DrawBox(box, { label: 'Anthony' })
+      drawBox.draw(canvas)
+    })
+  }
+}
+
+function recapture() {
+  location.reload()
+  document.querySelector('body').innerHTML = `
+    <h4 id="alert"></h4>
+  <video id="video" width="720" height="560" autoplay muted></video>
+
+        <div id="camera" style="height:auto;width:auto; text-align:center; display:none;"></div>
+  <br> 
+  `
+  startVideo()
 }
