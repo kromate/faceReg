654877b136f10a0d884358722df288364124caff
kromate <akpananthony33@gmail.com>
2022-02-20 22:37:00 +0100

    feat: mirgated

--- attendance-system/src/router/index.js
+++ attendance-system/src/router/index.js
@@ -1,5 +1,17 @@
-import Home from "../views/Home.vue";
+import Home from "@/views/home.vue";
+
+import { createWebHistory, createRouter } from "vue-router";
 
 const routes = [
-  { path: '/', component: Home },
+  { path: '/', 
+    name:"Home",
+   component: Home },
 ]
+
+
+ const router = createRouter({
+  history: createWebHistory(),
+  routes, 
+})
+
+export default router
