750389dc083918bff6befb98857575a10d50b7f1
kromate <akpananthony33@gmail.com>
2022-02-22 16:49:33 +0100

    feat: update

---
+++ attendance-system/src/firebase/init.js
@@ -0,0 +1,23 @@
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+
+var firebaseConfig = {
+
+};
+
+const firebaseApp = initializeApp({
+   apiKey: "AIzaSyCJPvMODeO5JuJe_VlUPZ7eF4tEqIoi1L8",
+  authDomain: "facereg-809b3.firebaseapp.com",
+  databaseURL: "https://facereg-809b3.firebaseio.com",
+  projectId: "facereg-809b3",
+  storageBucket: "facereg-809b3.appspot.com",
+  messagingSenderId: "741914714343",
+  appId: "1:741914714343:web:e24f447bb5727bfbd18242",
+  measurementId: "G-4JKN2R3DB2"
+});
+
+const analytics = getAnalytics(firebaseApp);
+export const db = getFirestore();
+
