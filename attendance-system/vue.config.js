739d3692d70c49088ff25aeb03633f3fd0681276
kromate <akpananthony33@gmail.com>
2022-02-25 00:24:28 +0100

    added vue cinfig

---
+++ attendance-system/vue.config.js
@@ -0,0 +1,10 @@
+
+const { GenerateSW } = require("workbox-webpack-plugin");
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === "development" ? "/vuejs-pwa/" : "",
+
+  configureWebpack: {
+    plugins: [new GenerateSW()]
+  }
+};
