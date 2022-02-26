a28cbc92e6f83a91151c20edc077798125931c1f
kromate <akpananthony33@gmail.com>
2022-02-26 23:46:54 +0100

    feat: update

--- attendance-system/src/main.js
+++
@@ -1,7 +0,0 @@
-import './helper/faceApi.min.js'
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import "./assets/main.css"
-
-createApp(App).use(router).mount('#app')
