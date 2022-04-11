0e74fc668119f76d8a4e23aeee1f450258555002
kromate <akpananthony33@gmail.com>
2022-04-11 22:49:23 +0100

    --- Auto-Git Commit ---

--- fas/src/views/register.vue
+++ fas/src/views/register.vue
@@ -1,11 +1,11 @@
 <template>
 	<div>
 		<h4 id="alert"></h4>
+
+	
 		<div class="web-cam">
 			<video id="video" ref="video" width="500" height="500" autoplay muted class=""></video>
 		</div>
-	
-
 		<div id="camera" ref="camera" class="h-auto w-auto text-center hidden"></div>
 		<br>
 		<p id="snapShot"></p>
@@ -27,14 +27,11 @@ onMounted(ScanFace)
 </script>
 
 <style scoped>
-.web-cam {
-
-  position: relative;
-  text-align: center;
-  width: 250px;
-  height: 250px;
-  border-radius: 125px;
-  margin: 0 auto;
+/* .web-cam { 
+	margin-inline: auto;
+  width: 450px;
+  height: 450px;
+  border-radius: 999999px;
   -webkit-mask-image: -webkit-radial-gradient(circle, white 100%, black 100%);
-}
+} */
 </style>
